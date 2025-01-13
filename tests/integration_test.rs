a34b3ab58593519c971561a4e2e31c5db4fb1a49
dandyvica <dandyvica@gmail.com>
2025-01-13 19:09:27 +0100

    prepare 0.5.1, fixed bug on OPT options, enhanced doc

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -22,13 +22,11 @@ fn test_dqy_call(args: &str) {
 }
 
 macro_rules! test_dqy {
-    ($fn_name:ident, $args:literal) => {
+    ($fn_name:ident, $args:literal, $gha:literal) => {
         #[test]
         fn $fn_name() {
             // don't use IPV6 on github action runners because it's not yet supported
-            if std::env::var("GITHUB_REPOSITORY").is_err() {
-                test_dqy_call($args);
-            } else if !$args.contains("-6") {
+            if $gha || std::env::var("GITHUB_REPOSITORY").is_err() {
                 test_dqy_call($args);
             }
         }
@@ -36,50 +34,53 @@ macro_rules! test_dqy {
 }
 
 // test transports
-test_dqy!(udp, "A www.google.com");
-test_dqy!(udp6, "A www.google.com -6");
-test_dqy!(tcp, "A www.google.com --tcp");
-test_dqy!(tcp6, "A www.google.com --tcp -6");
-test_dqy!(tls, "A www.google.com @1.1.1.1 --tls");
-test_dqy!(tls6, "A www.google.com @2606:4700:4700::1001 --tls");
+test_dqy!(udp, "A www.google.com", true);
+test_dqy!(udp6, "A www.google.com -6", false);
+test_dqy!(tcp, "A www.google.com --tcp", true);
+test_dqy!(tcp6, "A www.google.com --tcp -6", false);
+test_dqy!(tls, "A www.google.com @1.1.1.1 --tls", true);
+test_dqy!(tls6, "A www.google.com @2606:4700:4700::1001 --tls", false);
 test_dqy!(
     doh,
-    "A www.google.fr @https://mozilla.cloudflare-dns.com/dns-query --https"
+    "A www.google.fr @https://mozilla.cloudflare-dns.com/dns-query --https",
+    true
 );
 test_dqy!(
     doh6,
-    "A www.google.fr @https://mozilla.cloudflare-dns.com/dns-query --https -6"
+    "A www.google.fr @https://mozilla.cloudflare-dns.com/dns-query --https -6",
+    false
 );
-test_dqy!(doq, "A www.google.com @quic://dns.adguard.com");
-test_dqy!(doq6, "A www.google.com @quic://dns.adguard.com -6");
+test_dqy!(doq, "A www.google.com @quic://dns.adguard.com", true);
+test_dqy!(doq6, "A www.google.com @quic://dns.adguard.com -6", false);
 
 // endpoints
-test_dqy!(one, "A www.google.com @one.one.one.one");
-test_dqy!(one6, "A www.google.com @one.one.one.one -6");
-test_dqy!(one_tcp, "A www.google.com @one.one.one.one --tcp");
-test_dqy!(one_tcp6, "A www.google.com @one.one.one.one --tcp -6");
-test_dqy!(one_port, "A www.google.com @one.one.one.one:53");
-test_dqy!(one_port6, "A www.google.com @one.one.one.one:53 -6");
+test_dqy!(one, "A www.google.com @one.one.one.one", true);
+test_dqy!(one6, "A www.google.com @one.one.one.one -6", false);
+test_dqy!(one_tcp, "A www.google.com @one.one.one.one --tcp", true);
+test_dqy!(one_tcp6, "A www.google.com @one.one.one.one --tcp -6", false);
+test_dqy!(one_port, "A www.google.com @one.one.one.one:53", true);
+test_dqy!(one_port6, "A www.google.com @one.one.one.one:53 -6", false);
 
 // IDNA
-test_dqy!(german, "A münchen.de");
-test_dqy!(cyrillic, "A россия.рф");
-test_dqy!(greek, "AAAA ουτοπία.δπθ.gr");
-test_dqy!(korean, "A 스타벅스코리아.com");
-test_dqy!(nordic, "A www.øl.com");
-test_dqy!(chinese, "A 香港.中國");
+test_dqy!(german, "A münchen.de", true);
+test_dqy!(cyrillic, "A россия.рф", true);
+test_dqy!(greek, "AAAA ουτοπία.δπθ.gr", true);
+test_dqy!(korean, "A 스타벅스코리아.com", true);
+test_dqy!(nordic, "A www.øl.com", true);
+test_dqy!(chinese, "A 香港.中國", true);
 
 // Misc
-test_dqy!(dropbox, "TXT dropbox.com");
+test_dqy!(dropbox, "TXT dropbox.com", true);
 test_dqy!(
     zoneversion,
-    "@ns1-dyn.bortzmeyer.fr dyn.bortzmeyer.fr SOA --zoneversion"
+    "@ns1-dyn.bortzmeyer.fr dyn.bortzmeyer.fr SOA --zoneversion",
+    true
 );
-test_dqy!(wallet, "bortzmeyer.fr WALLET");
-test_dqy!(type262, "bortzmeyer.fr type262");
-test_dqy!(axfr, "axfr @nsztm1.digi.ninja zonetransfer.me");
-test_dqy!(any, "@a.gtld-servers.net ANY com.");
-test_dqy!(https, "HTTPS arts.fr");
-test_dqy!(caa, "CAA duckduckgo.com");
-test_dqy!(ptr, "-x 3.209.40.246");
-test_dqy!(mx, "mx yahoo.com");
+test_dqy!(wallet, "bortzmeyer.fr WALLET", true);
+test_dqy!(type262, "bortzmeyer.fr type262", true);
+test_dqy!(axfr, "axfr @nsztm1.digi.ninja zonetransfer.me", true);
+test_dqy!(any, "@a.gtld-servers.net ANY com.", true);
+test_dqy!(https, "HTTPS arts.fr", true);
+test_dqy!(caa, "CAA duckduckgo.com", true);
+test_dqy!(ptr, "-x 3.209.40.246", true);
+test_dqy!(mx, "mx yahoo.com", true);
